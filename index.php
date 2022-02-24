402d15186cd5b328a32f94a0256125eac09fb0d3
MMacedoS <macedo.mauriciogen@gmail.com>
2022-02-24 17:11:27 -0300

    aula 01

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imovies CEHMA</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <section class="box">
+        <header>
+            <div id="header_topo">               
+               <a href="index.php">
+                        <img src="images/cehmaimoveis_logo.png" alt="Home" title="Home">
+               </a>
+                <ul>
+                    <li><a href="#">Comprar</a></li>
+                    <li><a href="#">Alugar</a></li>
+                    <li><a href="#">Anunciar</a></li>
+                    <li><a href="#">Fale Conosco</a></li>
+                </ul>     
+
+            </div>
+            <div id="header_navegar">
+
+            </div>
+        </header>
+    </section>
+    <section class="box">
+        <div id="imoveis_home">
+
+        </div>
+        <div id="imoveis_home_lista">
+
+        </div>
+
+
+    </section>
+
+    <section id="footer">
+
+    </section>
+</body>
+</html>
